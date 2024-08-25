cda8eb7e2d5b341960c97e01fba2b299cb9fee84
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-08-26 01:01:03 +0500

    Fixed a typo in Post Form

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     status : false,
-    userData: null
+    userData: {}
 }
 
 const authSlice = createSlice({
@@ -15,7 +15,7 @@ const authSlice = createSlice({
         },
         logout: (state) => {
             state.status = false;
-            state.userData = null;
+            state.userData = {};
         }
      }
 })
